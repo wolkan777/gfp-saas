79ca428dfda0d0d3b907d6f2753f2a494c1bc2a0
Volkan Altuğ <wolkan777@gmail.com>
2020-04-24 16:03:06 +0300

    ilk sass

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var uglifycss = require('gulp-uglifycss');
+var autoprefixer = require('gulp-autoprefixer');
+
+function style() {
+    return gulp.src('./sass/main.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./css'))
+        .pipe(uglifycss({
+            "uglyComments": true
+        }))
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('./dist'))
+
+}
+
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+    gulp.watch('./sass/*.scss', style)
+    gulp.watch('./*.html').on('change', browserSync.reload);
+    gulp.watch('./js/*.js').on('change', browserSync.reload);
+}
+
+exports.style = style;
+exports.watch = watch;
